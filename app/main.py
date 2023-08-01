c2fbb49250984becdaaa2a87057f1622908284fd
Miqueias <miqueias.rihs@gmail.com>
2023-08-01 16:32:26 -0300

    template style

--- app/main.py
+++ app/main.py
@@ -3,18 +3,26 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from nasa.nasa_api import NasaAPI
+from .nasa.nasa_api import NasaAPI
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def render_page(request: Request):
     nasa = NasaAPI()
     response = nasa.get_image().json()
     
     return templates.TemplateResponse("main.html", {"request": request, "data": response})
+
+
+@app.get("/nasa-image")
+async def get_image(date: str):
+    nasa = NasaAPI()
+    response = nasa.get_image(date).json()
+    
+    return response
