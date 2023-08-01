e2ad2743a48064689977aaaf8c3005d4d2fb3595
Miqueias <miqueias.rihs@gmail.com>
2023-08-01 13:16:39 -0300

    first commit

---
+++ app/nasa/nasa_api.py
@@ -0,0 +1,14 @@
+import requests
+
+class NasaAPI:
+    def __init__(self):
+        self.api_key =  "OB8YqcfYVH6iH21Ch2ZKw64HUPwEsodHI2JW1Fsw"
+        self.url =  "https://api.nasa.gov/planetary/apod"
+        
+    
+    def get_image(self, birthday=''):
+        final_url = f'{self.url}?api_key={self.api_key}&date={birthday}'
+        response = requests.get(final_url)
+
+        return response
+        
