8804ff610ae08432f4fff17558c81433d5d2d215
Miqueias <miqueias.rihs@gmail.com>
2023-08-09 09:54:31 -0300

    Container fix

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from .nasa.nasa_api import NasaAPI
+from nasa.nasa_api import NasaAPI
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
